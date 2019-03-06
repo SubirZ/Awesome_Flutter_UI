0782ee672bdcffd9a3bb9f710504aa4b47b9309b
subir <ssuubbiirr@gmail.com>
2019-03-07 00:42:33 +0530

    Initial commit

---
+++ lib/inputWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class InputWidget extends StatelessWidget {
+  final double topRight;
+  final double bottomRight;
+
+  InputWidget(this.topRight, this.bottomRight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 40, bottom: 30),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 40,
+        child: Material(
+          elevation: 10,
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(bottomRight),
+                  topRight: Radius.circular(topRight))),
+          child: Padding(
+            padding: EdgeInsets.only(left: 40, right: 20, top: 10, bottom: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "JohnDoe@example.com",
+                  hintStyle: TextStyle(color: Color(0xFFE1E1E1), fontSize: 14)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_ui_bundle/background.dart';
+import 'package:flutter_ui_bundle/loginUi.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            Background(),
+            Login(),
+          ],
+        ));
+  }
+}
